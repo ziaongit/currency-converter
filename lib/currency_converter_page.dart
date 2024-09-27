cb3929bd6940d3d248d5ae5d91346f6bfc3e0aeb
ziaongit <zia_gt@yahoo.com>
2024-09-27 06:42:27 +0200

    Currency converter flutter

---
+++ lib/currency_converter_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterPage extends StatelessWidget {
+  const CurrencyConverterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        width: 2.0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.circular(5),
+    );
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          elevation: 0,
+          title: const Text('Currency Converter', style: TextStyle(color: Colors.white),),
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                '0',
+                style: TextStyle(
+                    fontSize: 45,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 255, 255, 255)),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  style: const TextStyle(color: Colors.black),
+                  keyboardType: const TextInputType.numberWithOptions(
+                    decimal: true,
+                  ),
+                  decoration: InputDecoration(
+                      hintText: 'Please enter the ammount in USD',
+                      hintStyle: const TextStyle(color: Colors.black),
+                      prefixIcon: const Icon(Icons.monetization_on),
+                      prefixIconColor: Colors.black,
+                      filled: true,
+                      fillColor: Colors.white,
+                      focusedBorder: border,
+                      enabledBorder: border),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                        minimumSize: const Size(double.infinity, 50),
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        )),
+                    child: const Text('Convert')),
+              )
+            ],
+          ),
+        ));
+  }
+}
